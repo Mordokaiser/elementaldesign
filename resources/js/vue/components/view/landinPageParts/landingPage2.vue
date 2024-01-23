2eab0cb83c51b61e831efce6f63485c908a36698
FTR20 <fabitapia9@gmail.com>
2024-01-22 21:52:25 -0300

    ED6 - crear landing page (2)
    Se ajustan detalles de la parte 2 de la landing page

--- resources/js/vue/components/view/landinPageParts/landingPage2.vue
+++ resources/js/vue/components/view/landinPageParts/landingPage2.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="poto">
-        <h2>NUESTROS SERVICIOS</h2>
+        <h2 class="textSebaServicios">NUESTROS <spam> SERVICIOS </spam></h2>
         <br><br><br>
         <div class="d-flex justify-content-evenly">
-            <div class="p-2">Flex item 1</div>
-            <div class="p-2">Flex item 2</div>
-            <div class="p-2">Flex item 3</div>
-            <div class="p-2">Flex item 4</div>
-            <div class="p-2">Flex item 5</div>
+            <div class="p-2"><img class="imagenServicio" :src="web"></div>
+            <div class="p-2"><img class="imagenServicio" :src="compra"></div>
+            <div class="p-2"><img class="imagenServicio" :src="seguridad"></div>
+            <div class="p-2"><img class="imagenServicio" :src="optimizacion"></div>
+            <div class="p-2"><img class="imagenServicio" :src="diseno"></div>
         </div>
         <br>
         <br>
@@ -15,13 +15,35 @@
 </template>
 
 <script setup>
+const web = '/images/landing/servicioWeb.png';
+const compra = '/images/landing/servicioCompra.png';
+const seguridad = '/images/landing/servicioSeguridad.png';
+const optimizacion = '/images/landing/servicioOptimizacion.png';
+const diseno = '/images/landing/servicioDiseno.png';
 </script>
 <style>
+.textSebaServicios {
+    padding-top: 50px;
+    font-family: 'Inter';
+    font-size: 20px;
+    color: rgb(255, 255, 255);
+    text-transform: uppercase;
+    letter-spacing: 0.2rem;
+
+}
+
+.textSebaServicios spam {
+    background-color: rgb(0, 141, 213);
+    font-family: 'inter';
+    color: #fff;
+    text-align: center;
+    text-transform: uppercase;
+    letter-spacing: 0.2rem;
+}
 .poto {
     padding: 10%;
     text-align: center;
     background-image: url('/images/backgrounds/landingBackground2.png');
-    color: aliceblue;
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
@@ -32,5 +54,9 @@
     background-blend-mode: color;
     background-color: rgba(0, 0, 0, 0.5);
     /* Cambia el valor de transparencia según tus necesidades */
+}
+.imagenServicio{
+    max-width: 240px;
+    max-height: 240px;
 }
 </style>
